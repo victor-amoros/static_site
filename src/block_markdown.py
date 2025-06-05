1ec40bc3d9ada0638809f7e6d4ebc8794a2428f7
victor-amoros <victor.amoros96@gmail.com>
2025-06-05 20:48:08 +0200

    added inline and block markdown files, added markdown to block and inline functions

---
+++ src/block_markdown.py
@@ -0,0 +1,46 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
+def markdown_to_blocks(markdown):
+    block_list = []
+    block_list = markdown.split("\n\n")
+    result = []
+    for block in block_list:
+        if block == "":
+            continue
+        result.append(block.strip())
+    return result
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.OLIST
+    return BlockType.PARAGRAPH
